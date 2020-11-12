fe1394d2157a92ad2dd4b5a7e530540eec476db7
Miguel Ángel <mdiasan28@gmail.com>
2020-11-12 10:59:54 +0100

    Primeras Plantillas añadidas

---
+++ includes/footer.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Ejercicio 8</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+    <div class="footer col-md-12">
+        <div class=" col-12">
+            <h1>Footer</h1>
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <div class=" contenedorPequeño col">
+                    C/Ronda del Valle nº32
+                </div>
+                <div class=" contenedorPequeño col">
+                    Nostromo@comics.com
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class=" contenedorPequeño col">
+                    Destacamos
+                </div>
+                <div class=" contenedorPequeño col">
+                </div>
+                <div class=" contenedorPequeño col">
+                </div>
+                <div class=" contenedorPequeño col">
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class=" contenedorPequeño col">
+                    Mi cuenta
+                </div>
+                <div class=" contenedorPequeño col">
+                </div>
+                <div class=" contenedorPequeño col">
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class=" contenedorPequeño col">
+                    Informacion
+                </div>
+                <div class=" contenedorPequeño col">
+                </div>
+                <div class=" contenedorPequeño col">
+                </div>
+                <div class=" contenedorPequeño col">
+                </div>
+                <div class=" contenedorPequeño col">
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
