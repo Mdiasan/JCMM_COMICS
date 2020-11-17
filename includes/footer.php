6458902cecc6c7870fb6eb04dc2cd147c36599f7
Miguel Ángel <mdiasan28@gmail.com>
2020-11-17 12:11:34 +0100

    Area Contacto

--- includes/footer.php
+++ includes/footer.php
@@ -1,31 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <title>Ejercicio 8</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <title>Ejercicio 8</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 
 </head>
+
 <body>
     <div class="footer col-md-12">
         <div class=" col-12">
             <h1>Footer</h1>
         </div>
         <div class="row">
-            <div class="col-md-3">
-                <div class=" contenedorPequeño col">
-                    C/Ronda del Valle nº32
-                </div>
-                <div class=" contenedorPequeño col">
-                    Nostromo@comics.com
-                </div>
-            </div>
-
-            <div class="col-md-3">
+            <div class="col-md-4">
                 <div class=" contenedorPequeño col">
                     Destacamos
                 </div>
@@ -37,7 +30,7 @@
                 </div>
             </div>
 
-            <div class="col-md-3">
+            <div class="col-md-4">
                 <div class=" contenedorPequeño col">
                     Mi cuenta
                 </div>
@@ -47,7 +40,7 @@
                 </div>
             </div>
 
-            <div class="col-md-3">
+            <div class="col-md-4">
                 <div class=" contenedorPequeño col">
                     Informacion
                 </div>
@@ -63,4 +56,5 @@
         </div>
     </div>
 </body>
+
 </html>
