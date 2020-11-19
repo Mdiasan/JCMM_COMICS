6f2a6f1d007dae4edf67ebf3332acdabf8cfc58f
Miguel Ángel <mdiasan28@gmail.com>
2020-11-19 11:14:17 +0100

    footer y contact

--- contact.php
+++ contact.php
@@ -8,32 +8,34 @@
 
 <body>
 
-	<?php include("includes/design-top.php"); ?>
+
 
 	<div class="container" id="main-content">
-		<div class="row">
-			<div class="col-sm-6">
-				<h2>Contactanos</h2>
-				<p>Rellena con tu correo electrónico para que te enviemos más información sobre nosotros</p>
-				<form class="form-inline col">
-					<input class="form-control col-sm-8" type="search" placeholder="@example.com" aria-label="Search">
-					<button class="btn btn-primary col-sm-8" type="submit">buscar</button>
-				</form>
+		<?php include("includes/design-top.php"); ?>
+		<main>
+			<div class="row">
+				<div class="col-sm-6 p-0">
+					<h2>Contactanos</h2>
+					<p>Rellena con tu correo electrónico para que te enviemos más información sobre nosotros</p>
+					<form class="form-inline col">
+						<input class="form-control col-sm-8" type="search" placeholder="@example.com" aria-label="Search">
+						<button class="btn btn-primary col-sm-8" type="submit">buscar</button>
+					</form>
+				</div>
+
+				<div class="col-sm-6">
+					<h2>Sobre nosotros</h2>
+					<p>Lucena(Cordoba) 14900 en C/Ronda del Valle nº15</p>
+					<p>Numero de atención al cliente 957364912</p>
+					<img src="media\images\logo-jcmm-128x128.png">
+				</div>
+
 			</div>
-
-			<div class="col-sm-6">
-			<h2>Sobre nosotros</h2>
-			<p>Lucena(Cordoba) 14900 en C/Ronda del Valle nº15</p>
-			<p>Numero de atención al cliente 957364912</p>
-			<img src="media\images\logo-jcmm-128x128.png">
-			</div>
-
-		</div>
-
-
+		</main>
+		<?php include("includes/footer.php"); ?>
 	</div>
 
-	<?php include("includes/footer.php"); ?>
+
 
 </body>
 
