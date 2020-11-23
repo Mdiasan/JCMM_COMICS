57c7a1524d9f9ce23c65fe70be67fcbb3e55b580
mramflo <m.ram.flo.ets@iesmaquesdecomares.org>
2020-11-23 11:10:39 +0100

    cambios : añadido fichero crear usuario y modificado el login

---
+++ crearUsuario.php
@@ -0,0 +1,51 @@
+<?php include("includes/a_config.php"); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php include("includes/head-tag-contents.php"); ?>
+</head>
+
+<body>
+    <div class="container">
+        <?php include("includes/design-top.php"); ?>
+
+        <main>
+
+
+
+
+
+            <div class="col p-0 ">
+                <h1>crear usuario</h1>
+                <div class="p-1">
+
+
+
+                    <form action="" class="form-group   ">
+                        <label for="usuario">usuario</label>
+                        <input type="text" name="usuario" class="form-control">
+                        <label for="nombre">nombre</label>
+                        <input type="text" name="nombre" class="form-control">
+                        <label for="apellidos">apellidos</label>
+                        <input type="text" name="apellidos" class="form-control">
+                        <label for="contraseña">contraseña</label>
+                        <input type="password" name="contraseña" class="form-control">
+                        <label for="recontraseña">repite la contraseña</label>
+                        <input type="password" name="recontraseña" class="form-control">
+                        <input type="submit" class="form-control-3 btn btn-warning mt-3" value="enviar">
+                    </form>
+                </div>
+            </div>
+
+
+
+        </main>
+
+
+
+        <?php include("includes/footer.php"); ?>
+    </div>
+</body>
+
+</html>
--- login.php
+++ login.php
@@ -13,42 +13,22 @@
         <main>
 
             <div class="row">
-                <div class="col-sm-6 p-0 ">
-                <h1>crear usuario</h1>
-                    <div class="p-1">
-                    
-                  
 
-                        <form action="" class="form-group   ">
-                            <label for="usuario">usuario</label>
-                            <input type="text" name="usuario" class="form-control">
-                            <label for="nombre">nombre</label>
-                            <input type="text" name="nombre" class="form-control">
-                            <label for="apellidos">apellidos</label>
-                            <input type="text" name="apellidos" class="form-control">
-                            <label for="contraseña">contraseña</label>
-                            <input type="password" name="contraseña" class="form-control">
-                            <label for="recontraseña">repite la contraseña</label>
-                            <input type="password" name="recontraseña" class="form-control">
-                            <input type="submit" class="form-control-3 btn btn-warning mt-3" value="enviar">
-                        </form>
-                    </div>
-                </div>
-                <div class="col-sm-6 p-0">
-                
+                <div class="col p-0">
+
                     <h1>logueate</h1>
                     <div class="p-1">
-                    <form action="" class="form-group">
+                        <form action="" class="form-group">
 
-                        <label for="usuario">usuario</label>
-                        <input type="text" name="usuario" class="form-control">
-                        <label for="contraseña">contraseña</label>
-                        <input type="password" name="contraseña" class="form-control">
-                        <input type="submit" class="form-control-3 btn btn-warning mt-3" value="enviar">
-
-                    </form>
+                            <label for="usuario">usuario</label>
+                            <input type="text" name="usuario" class="form-control">
+                            <label for="contraseña">contraseña</label>
+                            <input type="password" name="contraseña" class="form-control">
+                            <input type="submit" class="form-control-3 btn btn-warning mt-3" value="enviar">
+                            <a href="crearUsuario.php">no tienes cuenta registrate aquí!</a>
+                        </form>
                     </div>
-            
+
                 </div>
 
 
