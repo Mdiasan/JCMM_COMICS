085d71129e8092d69a90fe835aab44ab23864181
mramflo <m.ram.flo.ets@iesmaquesdecomares.org>
2020-11-25 10:34:08 +0100

    carrito

---
+++ carrito.php
@@ -0,0 +1,32 @@
+<?php include("includes/a_config.php"); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php include("includes/head-tag-contents.php"); ?>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <?php include("includes/design-top.php"); ?>
+
+        <main>
+
+          <div class="row">
+            <div class="col">
+                identificate
+            </div>
+            <div class="col">
+                carrito
+            </div>
+          </div>
+
+        </main>
+
+
+
+        <?php include("includes/footer.php"); ?>
+    </div>
+</body>
+
+</html>
