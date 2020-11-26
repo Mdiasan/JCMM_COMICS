37e25560bc69e49863fed702a526f25e142ad18d
Miguel Ángel <mdiasan28@gmail.com>
2020-11-26 12:41:27 +0100

    Cambio Footer

--- includes/navigation.php
+++ includes/navigation.php
@@ -9,7 +9,7 @@
   <div class="collapse navbar-collapse" id="navbar-list-2">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="login.php">Iniciar Sesion <span class="sr-only"></span></a>
+        <a class="nav-link" href="login.php"><i class="fas fa-user"></i> Iniciar Sesion <span class="sr-only"></span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="index.php">Inicio</a>
