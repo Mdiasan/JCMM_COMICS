0345f120f4094e82f506f95f22941fbff1c84044
mramflo <m.ram.flo.ets@iesmaquesdecomares.org>
2021-01-14 11:20:34 +0100

    video terminado

--- videos.php
+++ videos.php
@@ -1,16 +1,16 @@
 <?php include("includes/a_config.php"); ?>
 <!DOCTYPE html>
 <html>
+<div class="container-fluid">
 
-<head>
-	<?php include("includes/head-tag-contents.php"); ?>
+	<head>
+		<?php include("includes/head-tag-contents.php"); ?>
 
-</head>
+	</head>
 
-<body>
+	<body>
 
 
-	<div class="container-fluid">
 		<?php include("includes/design-top.php"); ?>
 		<div>
 			<div class="row">
@@ -18,28 +18,57 @@
 					<h4>Trailers Marvel</h4>
 				</div>
 			</div>
-			<div id="video-container">
-				<!-- Video -->
-				<video id="video" width="680" height="365">
-					<source src="media/Player/videos/Vengadores_ Endgame - Trailer español (HD).mp4" type="video/mp4">
-				</video>
-				<!-- Video Controls -->
-				<div id="video-controls">
-					<button type="button" id="play-pause" class="play">Play</button>
-					<input type="range" id="seek-bar" value="0">
-					<button type="button" id="mute">Mute</button>
-					<input type="range" id="volume-bar" min="0" max="1" step="0.1" value="1">
-					<button type="button" id="full-screen">Full-Screen</button>
-				</div>
-				<h3>Vengadores Endgame</h3>
-			</div>
 
-			<script src="js/video.js"></script>
+
+
+
 		</div>
-		<?php include("includes/footer.php"); ?>
-	</div>
 
+		
+
+			<div class="row">
+				<div class="col text-center ">
+					Avengers: Endgame (Vengadores: Endgame en España)3​ es una película de superhéroes estadounidense de 2019 basada en el grupo Los Vengadores de Marvel Comics,
+				</div>
+				<div class="col">
+					<div id="video-container">
+
+						<!-- Video -->
+						<video id="video" width="680" height="365">
+							<source src="media/Player/videos/Vengadores_ Endgame - Trailer español (HD).mp4" type="video/mp4">
+						</video>
+						<!-- Video Controls -->
+						<div id="video-controls">
+							<button type="button" id="play-pause" class="play">Play</button>
+							<input type="range" id="seek-bar" value="0">
+							<button type="button" id="mute">Mute</button>
+							<input type="range" id="volume-bar" min="0" max="1" step="0.1" value="1">
+							<button type="button" id="full-screen">Full-Screen</button>
+						</div>
+
+					</div>
+
+				</div>
+
+
+			</div>
+		
+
+
+
+
+
+
+
+
+
+
+		<?php include("includes/footer.php"); ?>
+
+</div>
 
 </body>
+
+<script src="js/video.js"></script>
 
 </html>
