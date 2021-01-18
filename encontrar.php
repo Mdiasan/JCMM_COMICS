ec5d8b5e19a037da94c7a759a424c8f0219b5293
Miguel Ángel <mdiasan28@gmail.com>
2021-01-18 11:16:20 +0100

    Videojuegos Acabados

--- encontrar.php
+++ encontrar.php
@@ -2,34 +2,32 @@
 <?php include("includes/a_config.php"); ?>
 
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php include("includes/head-tag-contents.php"); ?>
 
-    <title>juego</title>
-   <link rel="stylesheet" href="media/Videojuegos/Videojuego Miguel_Angel_Diaz/style.css">
-        
+    <title>Videojuego</title>
+    <link rel="stylesheet" href="media/Videojuegos/Videojuego Miguel_Angel_Diaz/style.css">
+
     <script src="media/Videojuegos/Videojuego Miguel_Angel_Diaz/memory.js" charset="utf-8"></script>
 
-   
 </head>
-<body >
 
-<div class="container-fluid">
+<body>
 
-<?php// include("includes/design-top.php"); ?>
+    <div class="container-fluid">
+        <main>
 
-<h1>Puntuación:<span id="result"></span></h1>
+            <h2>Puntuación:<span id="result"></span></h2>
 
-<div class="grid">
+            <div class="grid"></div>
+            <a href="index.php">Volver a Inicio</a>
+        </main>
+        <?php include("includes/footer.php"); ?>
+    </div>
+</body>
 
-    
-
-</div>
-<?php// include("includes/footer.php"); ?>
-</div>
-    </body>
-   
 
 </html>
