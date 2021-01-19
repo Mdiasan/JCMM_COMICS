c2b0fa2c46142e816f973f8b0b8da4f9305be5d1
Miguel Ángel <mdiasan28@gmail.com>
2021-01-19 10:53:17 +0100

    Video añadido

--- videos.php
+++ videos.php
@@ -5,11 +5,9 @@
 
 	<head>
 		<?php include("includes/head-tag-contents.php"); ?>
-
 	</head>
 
 	<body>
-
 
 		<?php include("includes/design-top.php"); ?>
 		<div>
@@ -19,16 +17,13 @@
 				</div>
 			</div>
 
-
-
-
 		</div>
 
 		
 
 			<div class="row">
 				<div class="col text-center ">
-					Avengers: Endgame (Vengadores: Endgame en España)3​ es una película de superhéroes estadounidense de 2019 basada en el grupo Los Vengadores de Marvel Comics,
+					Avengers: Endgame (Vengadores: Endgame en España) es una película de superhéroes estadounidense de 2019 basada en el grupo Los Vengadores de Marvel Comics
 				</div>
 				<div class="col">
 					<div id="video-container">
@@ -39,29 +34,44 @@
 						</video>
 						<!-- Video Controls -->
 						<div id="video-controls">
-							<button type="button" id="play-pause" class="play">Play</button>
+							<button type="button" id="play-pause" class="play">></button>
 							<input type="range" id="seek-bar" value="0">
-							<button type="button" id="mute">Mute</button>
+							<button type="button" id="mute"><i class="fas fa-volume-off"></i></button>
 							<input type="range" id="volume-bar" min="0" max="1" step="0.1" value="1">
-							<button type="button" id="full-screen">Full-Screen</button>
+							<button type="button" id="full-screen"><i class="fas fa-compress"></i></button>
 						</div>
 
 					</div>
 
 				</div>
+				<div class="row">
+				<div class="col text-center ">
+				Spider-Man: Lejos de casa (en inglés: Spider-Man: Far From Home) es una película estadounidense de superhéroes de 2019 basada en el personaje de Spider-Man, perteneciente a Marvel Comics, producida por Columbia Pictures y Marvel Studios y distribuida por Sony Pictures Entertainment. Es la secuela de Spider-Man: Homecoming(2017)
+				</div>
+
+				<div class="col">
+					<div id="video-container">
+
+						<!-- Video -->
+						<video id="video2" width="680" height="365">
+							<source src="media/Player/videos/SPIDER-MAN LEJOS DE CASA .mp4" type="video/mp4">
+						</video>
+						<!-- Video Controls -->
+						<div id="video-controls">
+							<button type="button" id="play-pause2" class="play">></button>
+							<input type="range" id="seek-bar2" value="0">
+							<button type="button" id="mute2"><i class="fas fa-volume-off"></i></button>
+							<input type="range" id="volume-bar2" min="0" max="1" step="0.1" value="1">
+							<button type="button" id="full-screen2"><i class="fas fa-compress"></i></button>
+						</div>
+
+					</div>
+
+				</div>
+				</div>
 
 
 			</div>
-		
-
-
-
-
-
-
-
-
-
 
 		<?php include("includes/footer.php"); ?>
 
