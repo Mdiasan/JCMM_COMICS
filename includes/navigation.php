5e7ec390d989f975059d2f881c45e6c789d71383
mramflo <m.ram.flo.ets@iesmaquesdecomares.org>
2021-01-28 12:58:44 +0100

    act

--- includes/navigation.php
+++ includes/navigation.php
@@ -24,13 +24,8 @@
         </div>
       </li>
 
-<<<<<<< HEAD
       <li class="nav-item dropdown d-none d-md-block">
-        <a class="nav-link dropdown-toggle" href="libros.php" id="navbardrop" data-toggle="dropdown">
-=======
-      <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
->>>>>>> dff513b80a7fd71ceab2afe72686eb8cf1172ec3
           Juegos
         </a>
         <div class="dropdown-menu">
