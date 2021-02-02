a96bf3f81aaf9bc4d85a748da81f4f949f08ce0d
mramflo <m.ram.flo.ets@iesmaquesdecomares.org>
2021-02-02 13:02:50 +0100

    LOGEOS

--- crearUsuario.php
+++ crearUsuario.php
@@ -12,22 +12,44 @@
 
         <main>
 
-            <div class="col p-0 ">
+            <div class="col ">
                 <h1>Crear usuario</h1>
-                <div class="p-1">
+                <div class="formLogeo">
 
-                    <form action="" class="form-group   ">
+                    <form action=""  class="needs-validation"   method="POST" novalidate  >
+                    <div class="form-group">
                         <label for="usuario">Usuario</label>
-                        <input type="text" name="usuario" class="form-control">
+                        <input type="text" name="usuario" class="form-control" required>
+                    </div>
+                    <div class="form-group">
                         <label for="nombre">Nombre</label>
-                        <input type="text" name="nombre" class="form-control">
+                        <input type="text" name="nombre" class="form-control" required>
+                    </div>
+                    <div class="form-group">
                         <label for="apellidos">Apellidos</label>
-                        <input type="text" name="apellidos" class="form-control">
+                        <input type="text" name="apellidos" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="apellidos">Mail</label>
+                        <input type="text" name="mail" class="form-control" required>
+                    </div>
+                    <div class="form-group"> 
                         <label for="contraseña">Contraseña</label>
-                        <input type="password" name="contraseña" class="form-control">
+                        <input type="password" name="contraseña" class="form-control"    id="contraseña" required>
+                    </div>
+                    <div class="form-group">
                         <label for="recontraseña">Repite la contraseña</label>
-                        <input type="password" name="recontraseña" class="form-control">
-                        <input type="submit" class="form-control-3 btn btn-warning mt-3" value="Enviar">
+                        <input type="password" name="recontraseña"   id="recontraseña"class="form-control"     required>
+                       
+                        <div class="invalid-feedback"></div>
+                        <div class="errorDiv" id="error"> Las contraseñas no coinciden por favor revisalo </div>
+                        
+
+                    </div>
+                        <div class="text-center"> 
+                            <input class="form-control-3 btn btn-warning mt-3  " type="submit" id="btn"  value="Enviar"  disabled >
+
+                        </div>
                     </form>
                 </div>
             </div>
@@ -37,4 +59,51 @@
         <?php include("includes/footer.php"); ?>
     </div>
 </body>
+<script>
+// Disable form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Get the forms we want to add validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+</script>
+
+<script>
+   function validarContraseña(){
+        var contrasena= document.getElementById('contraseña').value;
+        var repite = document.getElementById('recontraseña').value;
+        var btn = document.getElementById('btn');
+
+
+        if(contrasena!=''){
+
+       
+        if(contrasena == repite){
+            document.getElementById('error').style.display='none';
+            btn.disabled=  false;
+        }else{
+            document.getElementById('error').style.display='block';
+            btn.disabled=  true;
+        }
+
+    }else{
+        document.getElementById('error').style.display='none';
+    }
+    }
+
+
+    setInterval('validarContraseña()',10);
+</script>
 </html>
--- login.php
+++ login.php
@@ -14,31 +14,48 @@
 
             <div class="row">
 
-                <div class="col p-0">
+                <div class="col ">
 
+                   
+                    <div class="p-1 formLogeo">
                     <h1>logueate</h1>
-                    <div class="p-1">
-                        <form action="" class="form-group" method="POST">
+                        <form action=""  class="needs-validation"   method="POST" novalidate>
 
-                            <label for="usuario">Usuario</label>
-                            <input type="text" name="usuario" class="form-control">
-                            <label for="contraseña">Contraseña</label>
-                            <input type="password" name="contraseña" class="form-control">
-                            <input type="submit" class="form-control-3 btn btn-warning mt-3" value="Enviar">
-                            
-                         
+
+                            <div class="form-group"> <!--  DIV GROUP DE USUARIO-->
+                               
+                                <label for="usuario">Usuario</label>
+                                <input type="text" name="usuario" class="form-control" required>
+                                <div class="valid-feedback">Valid.</div>
+                                <div class="invalid-feedback">Please fill out this field.</div>
+                            </div> <!-- FIN  DIV GROUP DE USUARIO-->
+                            <div class="form-group"> <!--  DIV GROUP DE CONTRASEÑA-->
+                                <label for="contraseña">Contraseña</label>
+                                <input type="password" name="contraseña" class="form-control" required>
+                                <div class="valid-feedback">Valid.</div>
+                                <div class="invalid-feedback">Please fill out this field.</div>
+                            </div>  <!-- fin DIV GROUP DE CONTRASEÑA-->
+                         <div class="text-center">
+                         <input type="submit" class="form-control-3 btn btn-warning mt-3" value="Enviar">
+                        </div>
+
+
+
+
                         </form>
                     </div>
                     <br>
-                    <div id="gSignInWrapper">
-                            <span > o  Inicia sesíon con Google:</span>
-                            <br>,
+                    <div id="gSignInWrapper" class="formLogeo text-center">
+                        <span>Inicia sesíon con Google:</span>
+                        <br>
                         <div id="customBtn" class="customGPlusSignIn">
                             <span class="icon"></span>
                             <span class="buttonText">Google</span>
                         </div>
                     </div>
-                    <br> ¿No tienes cuenta? registrate <a href="crearUsuario.php">AQUÍ!</a>
+                    <div class="text-center">
+                         <br> ¿No tienes cuenta? registrate <a href="crearUsuario.php">AQUÍ!</a>
+                    </div>
 
                 </div>
 
@@ -52,5 +69,24 @@
         <?php include("includes/footer.php"); ?>
     </div>
 </body>
-
+<script>
+// Disable form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Get the forms we want to add validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+</script>
 </html>
