ef262df27115a921d734a8c7f438a8e2a4a4fbfe
Miguel Ángel <mdiasan28@gmail.com>
2021-02-02 11:31:02 +0100

    cc

--- bbdd/jcmmcomics.sql
+++ bbdd/jcmmcomics.sql
@@ -1,8 +1,27 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: jcmm-comics
+-- ------------------------------------------------------
+-- Server version	8.0.19
 
-create database jcmm-comics;
-use jcmm-comics;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `comic`
+--
+
 DROP TABLE IF EXISTS `comic`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `comic` (
   `id` int NOT NULL,
   `titulo` varchar(45) DEFAULT NULL,
@@ -15,14 +34,15 @@ CREATE TABLE `comic` (
   KEY `fk_comic_editorial1_idx` (`editorial_id`),
   CONSTRAINT `fk_comic_editorial1` FOREIGN KEY (`editorial_id`) REFERENCES `editorial` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `comic`
 --
 
 LOCK TABLES `comic` WRITE;
-
+/*!40000 ALTER TABLE `comic` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comic` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -30,20 +50,22 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `editorial`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `editorial` (
   `id` int NOT NULL,
   `nombre` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `editorial`
 --
 
 LOCK TABLES `editorial` WRITE;
-
+/*!40000 ALTER TABLE `editorial` DISABLE KEYS */;
+/*!40000 ALTER TABLE `editorial` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -51,7 +73,8 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `usuario`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuario` (
   `id` int NOT NULL,
   `usuario` varchar(40) DEFAULT NULL,
@@ -60,16 +83,18 @@ CREATE TABLE `usuario` (
   `apellido` varchar(100) DEFAULT NULL,
   `rol` varchar(45) DEFAULT NULL,
   `fecha_creacion` date DEFAULT NULL,
+  `mail` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `usuario`
 --
 
 LOCK TABLES `usuario` WRITE;
-
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -77,7 +102,8 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `valoracion`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `valoracion` (
   `id` int NOT NULL,
   `valoracion` int DEFAULT NULL,
@@ -90,9 +116,24 @@ CREATE TABLE `valoracion` (
   CONSTRAINT `fk_valoraciones_comics1` FOREIGN KEY (`comics_id`) REFERENCES `comic` (`id`),
   CONSTRAINT `fk_valoraciones_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `valoracion`
+--
 
 LOCK TABLES `valoracion` WRITE;
-
+/*!40000 ALTER TABLE `valoracion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `valoracion` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-02-02 11:30:52
