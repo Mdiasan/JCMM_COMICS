7b8542cb6552b51e0a467d798e07f0ebce5c1edb
JCCubero <71818389+JCCubero@users.noreply.github.com>
2021-02-10 10:19:54 +0100

    avance de las coockies

---
+++ includes/contenedorCookie.php
@@ -0,0 +1,13 @@
+
+
+<div class="alert text-center cookiealert" role="alert">
+            Si lo desea puede pasar directamente SIEMPRE a la pantalla de login 
+            <button type="button" class="btn btn-primary btn-sm acceptcookies" aria-label="Close">
+                Deseo ir directamente a la pantalla de login
+            </button>
+            <button id="rejectcookie" type="button" class="btn btn-danger btn-sm" aria-label="Close">
+                NO Deseo ir directamente a la pantalla de login
+            </button>
+        </div>
+
+   
