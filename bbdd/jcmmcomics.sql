10efbc1ac7c4ac87b1b3eaf2bde1890bcd7bc710
Miguel Ángel <mdiasan28@gmail.com>
2021-02-16 12:33:15 +0100

    cambio bdd

--- bbdd/jcmmcomics.sql
+++ bbdd/jcmmcomics.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `comic`
 --
-CREATE DATABASE IF NOT EXISTS `jcmm-comics`;
+CREATE DATABASE IF NOT EXISTS `jcmm-comics` DEAFULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `jcmm-comics`;
 DROP TABLE IF EXISTS `comic`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
